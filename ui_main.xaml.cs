94f36550ce9abeefdb178fb662e5dfb30fd61083
Rammaken <laggamer19573@gmail.com>
2024-10-11 22:46:30 -0400

    Added gamedata unpacker GUI components

--- ui_main.xaml.cs
+++ ui_main.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+
 namespace xrToolkit
 {
     /// <summary>
@@ -22,6 +26,85 @@ namespace xrToolkit
         public ui_main()
         {
             InitializeComponent();
+        }
+
+        private void btn_decompile_map_Click(object sender, RoutedEventArgs e)
+        {
+            if(input_map_name.Text.Equals("") || input_map_name.Text.Equals(null))
+            {
+                System.Windows.MessageBox.Show("Insert the level name", "Invalid action", MessageBoxButton.OK, MessageBoxImage.Warning);
+            } else
+            {
+                string var_level_name = input_map_name.Text;
+                // Ruta del archivo .exe
+                string exePath = @"toolkit/converter.exe";
+
+                // Argumentos que deseas pasar al .exe
+                string arguments = "-level sdk:" + var_level_name + " -mode le -with_lods";
+                System.Windows.MessageBox.Show(exePath + " " + arguments);
+
+                // Crear un nuevo proceso
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = exePath,
+                    Arguments = arguments,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = false
+                };
+
+                try
+                {
+                    // Iniciar el proceso
+                    using (Process process = Process.Start(startInfo))
+                    {
+                        // Leer la salida del proceso
+                        using (StreamReader reader = process.StandardOutput)
+                        {
+                            string result = reader.ReadToEnd();
+                            System.Windows.MessageBox.Show(result);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    {
+                        System.Windows.MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+
+
+                }
+            }
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(dialog.SelectedPath))
+            {
+                string path = dialog.SelectedPath;
+                input_db_input.Text = path;
+            }
+        }
+
+        private void btn_output_db_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(dialog.SelectedPath))
+            {
+                string path = dialog.SelectedPath;
+                input_db_output.Text = path;
+            }
         }
     }
 }
